3b2a74488afe11dd2bf6c15aaa0aa36b1f772299
potix <poti.dog@gmail.com>
2018-10-18 15:42:27 +0900

    update

--- actor_plugins/sender/configurator/config.go
+++ actor_plugins/sender/configurator/config.go
@@ -1,7 +1,7 @@
 package configurator
 
 type Config struct {
-    AddrPort
-    Label
-    FlushInterval
+    AddrPort string `json:"addr_port" yaml:"addr_port" toml:"addr_port"`
+    Label string `json:"label" yaml:"label" toml:"label"`
+    FlushInterval uint32 `json:"flush_interval" yaml:"flush_interval" toml:"flush_interval"`
 }
